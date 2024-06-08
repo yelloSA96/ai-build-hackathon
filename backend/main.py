20926d19d1ba85ad0ecc6fcaad8641a837a6a322
Thomas Suebwicha <t.suebwicha@gmail.com>
2024-06-08 19:46:58 +1000

    Adding flask component

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
-import sys
-import os
+import os, sys
 from dotenv import load_dotenv
 
 load_dotenv(".env")
@@ -7,8 +6,11 @@ load_dotenv(".env")
 PROJECT_DIRECTORY = os.getenv("PROJECT_DIRECTORY")
 sys.path.insert(1, PROJECT_DIRECTORY)
 
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 from langchain_core.messages import HumanMessage
-# from state import State
 from choose_philosophy import choose_philosophy
 from eastern_guru import eastern_guru
 from stoic_tutor import stoic_tutor
@@ -17,20 +19,34 @@ from diagnostician import diagnostician
 from prescriber import prescriber
 from happiness import happy_agent
 
-prompt = HumanMessage(content="I think I've had enough", name="human")
-philosopher = "existentialist"
 
-state = {
-    "messages": [prompt],
-    "philosopher": philosopher,
-}
+@app.route('/api', methods=["POST"])
+def getting_ai_response():
+    data = request.json
+    print(data)
+    human_query = data.get('human_query')
+    prompt = HumanMessage(content=human_query, name="human")
+    philosopher = data.get('philosopher')
 
-state = choose_philosophy(state)
-print("### philosophy response: ", state)
-state = happy_agent(state)
-print("\n### happy score: ", state)
-state = diagnostician(state)
-print("\n### diagnosis: ", state)
-state = prescriber(state)
-print("\n### prescription: ", state)
+    state = {
+        "messages": [prompt],
+        "philosopher": philosopher,
+    }
 
+    state = choose_philosophy(state)
+    print("### philosophy response: ", state)
+    state = happy_agent(state)
+    print("\n### happy score: ", state)
+    state = diagnostician(state)
+    print("\n### diagnosis: ", state)
+    state = prescriber(state)
+
+    last_message = state["messages"][-1].content
+    happiness_score = state["happiness_score"]
+
+    print("\n### prescription: ", state)
+    return jsonify(last_message,happiness_score), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
