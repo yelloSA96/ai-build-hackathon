7025c3d1c179396f59b68ee0f13c78d5702eab7d
Shannon Gray <shanngray@icloud.com>
2024-06-08 14:04:13 +1000

    backend commit

--- backend/prescriber.py
+++ backend/prescriber.py
@@ -1,6 +1,5 @@
-"""
-Module for analysing hapiness
-"""
+"prescriber"
+
 import sys
 import os
 from dotenv import load_dotenv
@@ -17,23 +16,24 @@ from langchain_core.messages import HumanMessage
 
 
 
-def happy_agent(input: str):
+def prescriber(input: str):
     
     prompt = HumanMessage(content=input, name="human")
     
     system_prompt = (
-        "# ROLE: You are an expert at analysing the happiness of a person. \n\n"
-        "# TASK: You will be presented with a conversation between a human and their philosopher. "
-        " Your job is to analyse the conversation and rate the humans happiness.\n\n"
+        "# ROLE: You are an expert at prescribing treatments. \n\n"
+        "# TASK: Your task is to prescribe a treatment for the human based on their diagnosis. \n"
         "# NOTES:\n"
-        " - Always return a numerical value."
-        " - Whole numbers only."
-        " - Negative values are sad."
-        " - Positive values are happy."
+        " - You always return a treatment and only a treatment."
+        " - You are known for being thorough and accurate."
+        " - You are known for being honest and transparent."
+        " - You are known for being empathetic and understanding."
+        " - You are known for being patient and understanding."
+        " - You are known for being helpful and supportive."
     )
     llm = ChatCohere(model_name="command-r-plus", temperature=1)
     
-    happy_prompt = ChatPromptTemplate.from_messages(
+    prescriber_prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
@@ -45,13 +45,12 @@ def happy_agent(input: str):
 
     output_parser = StrOutputParser()
  
-    happy_chain = happy_prompt | llm | output_parser
+    prescriber_chain = prescriber_prompt | llm | output_parser
 
-    happy_output = happy_chain.invoke({"messages": [prompt]})
+    prescriber_output = prescriber_chain.invoke({"messages": [prompt]})
     
     # Wrap the rewritten prompt in a HumanMessage object for standardized handling
  
-    return happy_output
+    return prescriber_output
 
-
-print(happy_agent("Hi, I'm feeling sad."))
+print(prescriber("You have a broken foot."))
